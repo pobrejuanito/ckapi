8a433b7a75c66549c847d71800bc6fb0fcc02b45
Ki SOng <pobrejuanito@gmail.com>
2019-01-15 04:09:22 +0000

    Initial

---
+++ ckapi/local_settings.py
@@ -0,0 +1,13 @@
+import os
+from .settings import *
+
+ALLOWED_HOSTS = ['api.cksda.church']
+
+SECRET_KEY = 's-$kz#!*q$s&eo5q=*1hf2k#v8p^lr-k67=n*@w3*8vwa%417!'
+
+DEBUG = True 
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+#    BASE_DIR + '/apifiles/static',
+#)
